b2445300242a404dd4b44ba3e8535ba0602dd45d
wanyingl09 <wli30@stuy.edu>
2022-09-20 20:07:06 -0400

    copy questions to file

--- 03_py/warmup2/cbwu2.py
+++ 03_py/warmup2/cbwu2.py
@@ -1,3 +1,23 @@
+'''
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+
+Expected	Run		
+string_times('Hi', 2) → 'HiHi'	'HiHi'	OK	
+string_times('Hi', 3) → 'HiHiHi'	'HiHiHi'	OK	
+string_times('Hi', 1) → 'Hi'	'Hi'	OK	
+string_times('Hi', 0) → ''	''	OK	
+string_times('Hi', 5) → 'HiHiHiHiHi'	'HiHiHiHiHi'	OK	
+string_times('Oh Boy!', 2) → 'Oh Boy!Oh Boy!'	'Oh Boy!Oh Boy!'	OK	
+string_times('x', 4) → 'xxxx'	'xxxx'	OK	
+string_times('', 4) → ''	''	OK	
+string_times('code', 2) → 'codecode'	'codecode'	OK	
+string_times('code', 3) → 'codecodecode'	'codecodecode'	OK	
+'''
 def string_times(str, n):
   if n == 0:
     return ""
@@ -7,6 +27,23 @@ def string_times(str, n):
     n = n - 1
   return string
 
+'''
+Given a string and a non-negative int n, we'll say that the front of the string is the first 3 chars, or whatever is there if the string is less than length 3. Return n copies of the front;
+
+
+front_times('Chocolate', 2) → 'ChoCho'
+front_times('Chocolate', 3) → 'ChoChoCho'
+front_times('Abc', 3) → 'AbcAbcAbc'
+
+Expected	Run		
+front_times('Chocolate', 2) → 'ChoCho'	'ChoCho'	OK	
+front_times('Chocolate', 3) → 'ChoChoCho'	'ChoChoCho'	OK	
+front_times('Abc', 3) → 'AbcAbcAbc'	'AbcAbcAbc'	OK	
+front_times('Ab', 4) → 'AbAbAbAb'	'AbAbAbAb'	OK	
+front_times('A', 4) → 'AAAA'	'AAAA'	OK	
+front_times('', 4) → ''	''	OK	
+front_times('Abc', 0) → ''	''	OK
+'''
 def front_times(str, n):
   front = str[:3]
   string = ""
@@ -15,6 +52,26 @@ def front_times(str, n):
     n = n - 1
   return string
 
+'''
+Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
+
+
+string_bits('Hello') → 'Hlo'
+string_bits('Hi') → 'H'
+string_bits('Heeololeo') → 'Hello'
+
+Expected	Run		
+string_bits('Hello') → 'Hlo'	'Hlo'	OK	
+string_bits('Hi') → 'H'	'H'	OK	
+string_bits('Heeololeo') → 'Hello'	'Hello'	OK	
+string_bits('HiHiHi') → 'HHH'	'HHH'	OK	
+string_bits('') → ''	''	OK	
+string_bits('Greetings') → 'Getns'	'Getns'	OK	
+string_bits('Chocoate') → 'Coot'	'Coot'	OK	
+string_bits('pi') → 'p'	'p'	OK	
+string_bits('Hello Kitten') → 'HloKte'	'HloKte'	OK	
+string_bits('hxaxpxpxy') → 'happy'	'happy'	OK	
+'''
 def string_bits(str):
   string = ""
   index = 0
@@ -23,6 +80,26 @@ def string_bits(str):
     index += 2
   return string
 
+'''
+Given a non-empty string like "Code" return a string like "CCoCodCode".
+
+
+string_splosion('Code') → 'CCoCodCode'
+string_splosion('abc') → 'aababc'
+string_splosion('ab') → 'aab'
+
+Expected	Run		
+string_splosion('Code') → 'CCoCodCode'	'CCoCodCode'	OK	
+string_splosion('abc') → 'aababc'	'aababc'	OK	
+string_splosion('ab') → 'aab'	'aab'	OK	
+string_splosion('x') → 'x'	'x'	OK	
+string_splosion('fade') → 'ffafadfade'	'ffafadfade'	OK	
+string_splosion('There') → 'TThTheTherThere'	'TThTheTherThere'	OK	
+string_splosion('Kitten') → 'KKiKitKittKitteKitten'	'KKiKitKittKitteKitten'	OK	
+string_splosion('Bye') → 'BByBye'	'BByBye'	OK	
+string_splosion('Good') → 'GGoGooGood'	'GGoGooGood'	OK	
+string_splosion('Bad') → 'BBaBad'	'BBaBad'	OK
+'''
 def string_splosion(str):
   string = ""
   index = 1
@@ -31,6 +108,29 @@ def string_splosion(str):
     index += 1
   return string
 
+'''
+Given a string, return the count of the number of times that a substring length 2 appears in the string and also as the last 2 chars of the string, so "hixxxhi" yields 1 (we won't count the end substring).
+
+
+last2('hixxhi') → 1
+last2('xaxxaxaxx') → 1
+last2('axxxaaxx') → 2
+
+Expected	Run		
+last2('hixxhi') → 1	1	OK	
+last2('xaxxaxaxx') → 1	1	OK	
+last2('axxxaaxx') → 2	2	OK	
+last2('xxaxxaxxaxx') → 3	3	OK	
+last2('xaxaxaxx') → 0	0	OK	
+last2('xxxx') → 2	2	OK	
+last2('13121312') → 1	1	OK	
+last2('11212') → 1	1	OK	
+last2('13121311') → 0	0	OK	
+last2('1717171') → 2	2	OK	
+last2('hi') → 0	0	OK	
+last2('h') → 0	0	OK	
+last2('') → 0	0	OK	
+'''
 def last2(str):
   last = str[len(str)-2:len(str)]
   retCount = 0
@@ -44,6 +144,23 @@ def last2(str):
     
   return retCount
 
+'''
+Given an array of ints, return the number of 9's in the array.
+
+
+array_count9([1, 2, 9]) → 1
+array_count9([1, 9, 9]) → 2
+array_count9([1, 9, 9, 3, 9]) → 3
+
+Expected	Run		
+array_count9([1, 2, 9]) → 1	1	OK	
+array_count9([1, 9, 9]) → 2	2	OK	
+array_count9([1, 9, 9, 3, 9]) → 3	3	OK	
+array_count9([1, 2, 3]) → 0	0	OK	
+array_count9([]) → 0	0	OK	
+array_count9([4, 2, 4, 3, 1]) → 0	0	OK	
+array_count9([9, 2, 4, 3, 1]) → 1	1	OK
+'''
 def array_count9(nums):
   counter = 0
   for e in nums:
@@ -51,6 +168,28 @@ def array_count9(nums):
       counter += 1
   return counter
 
+'''
+Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
+
+
+array_front9([1, 2, 9, 3, 4]) → True
+array_front9([1, 2, 3, 4, 9]) → False
+array_front9([1, 2, 3, 4, 5]) → False
+
+Expected	Run		
+array_front9([1, 2, 9, 3, 4]) → True	True	OK	
+array_front9([1, 2, 3, 4, 9]) → False	False	OK	
+array_front9([1, 2, 3, 4, 5]) → False	False	OK	
+array_front9([9, 2, 3]) → True	True	OK	
+array_front9([1, 9, 9]) → True	True	OK	
+array_front9([1, 2, 3]) → False	False	OK	
+array_front9([1, 9]) → True	True	OK	
+array_front9([5, 5]) → False	False	OK	
+array_front9([2]) → False	False	OK	
+array_front9([9]) → True	True	OK	
+array_front9([]) → False	False	OK	
+array_front9([3, 9, 2, 3, 3]) → True	True	OK	
+'''
 def array_front9(nums):
   counter = 0
   for e in nums:
@@ -59,6 +198,26 @@ def array_front9(nums):
     counter += 1
   return False
 
+'''
+Given an array of ints, return True if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+
+array123([1, 1, 2, 3, 1]) → True
+array123([1, 1, 2, 4, 1]) → False
+array123([1, 1, 2, 1, 2, 3]) → True
+
+Expected	Run		
+array123([1, 1, 2, 3, 1]) → True	True	OK	
+array123([1, 1, 2, 4, 1]) → False	False	OK	
+array123([1, 1, 2, 1, 2, 3]) → True	True	OK	
+array123([1, 1, 2, 1, 2, 1]) → False	False	OK	
+array123([1, 2, 3, 1, 2, 3]) → True	True	OK	
+array123([1, 2, 3]) → True	True	OK	
+array123([1, 1, 1]) → False	False	OK	
+array123([1, 2]) → False	False	OK	
+array123([1]) → False	False	OK	
+array123([]) → False	False	OK
+'''
 def array123(nums):
   counter = 0
   while counter < len(nums):
@@ -67,6 +226,26 @@ def array123(nums):
     counter += 1
   return False
 
+'''
+Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.
+
+
+string_match('xxcaazz', 'xxbaaz') → 3
+string_match('abc', 'abc') → 2
+string_match('abc', 'axc') → 0
+
+Expected	Run		
+string_match('xxcaazz', 'xxbaaz') → 3	3	OK	
+string_match('abc', 'abc') → 2	2	OK	
+string_match('abc', 'axc') → 0	0	OK	
+string_match('hello', 'he') → 1	1	OK	
+string_match('he', 'hello') → 1	1	OK	
+string_match('h', 'hello') → 0	0	OK	
+string_match('', 'hello') → 0	0	OK	
+string_match('aabbccdd', 'abbbxxd') → 1	1	OK	
+string_match('aaxxaaxx', 'iaxxai') → 3	3	OK	
+string_match('iaxxai', 'aaxxaaxx') → 3	3	OK
+'''
 def string_match(a, b):
   retCount = 0
   counter = 0
@@ -75,3 +254,4 @@ def string_match(a, b):
       retCount += 1
     counter += 1
   return retCount
+
