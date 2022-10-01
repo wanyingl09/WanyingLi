2b7e6c0d2fedba82829d421fcfc324a2f0fb58c4
wanyingl09 <wli30@stuy.edu>
2022-10-01 13:56:18 -0400

    copied april's files

---
+++ 04_choose/krewes.py
@@ -0,0 +1,70 @@
+'''
+April Li
+SoftDev
+K04 -- Dictionaries + RNG
+2022-09-23
+time spent: 1 hr
+DISCO:
+ - we learned how to reference dictionaries
+ - we learned how to import random and use it
+ - we learned how to make test cases :D
+ - list() to replace a list of keys
+QCC:
+ - Is there way to create an object in python like in java, where we can create multiple tests objects and test them off of a static test case?
+OPS SUMMARY:
+ 1) Take in a dictionary
+ 2) Randomly select a key from the dictionary
+ 3) Randomly select a element from the list assigned to the key
+ 4) Return the key and the element
+'''
+
+import random as random
+    
+#Randomly choose a student name out of all periods
+def choose(dictionary):
+    #Randomly select a key from the available keys in the dictionary
+    key = list(dictionary)[random.randint(0,len(list(dictionary))-1)]
+    #return(random.choice(dictionary[list(dictionary)[random.randint(0,len(list(dictionary))-1)]]))
+    
+    #if class is not empty
+    if len(dictionary[key]) > 0:
+        #Return the randomly selected key along with a randomly selected element in the list associated with the key
+        return str(key) + ": " + random.choice(dictionary[key])
+    return "class " + str(key) + " is empty"
+
+def addPd(pd, dictionary):
+    dictionary[pd] = []
+
+def rmPd(pd, dictionary):
+    return dictionary.pop(pd)
+
+def addStu(name, pd, dictionary):
+    dictionary[pd].append(name)
+    
+def rmStu(name, pd, dictionary):
+    for e in range(len(dictionary[pd])):
+        if dictionary[pd][e] == name:
+            return dictionary[pd].pop(e)
+    return "no student with " + name + " found"
+
+##TEST CASE
+# pd2 = ["john", "bob"]
+# pd7 = ["calvin", "samantha"]
+# pd8 = ["man", "woman"]
+
+# krewes1 = {2:pd2, 7:pd7, 8:pd8}
+krewes = {
+    2:["NICHOLAS",  "ANTHONY",  "BRIAN",  "SAMUEL",  "JULIA",  "YUSHA",  "CORINA",  "CRAIG",  "FANG MIN",  "JEFF",  "KONSTANTIN",  "AARON",  "VIVIAN",  "AYMAN",  "TALIA",  "FAIZA",  "ZIYING",  "YUK KWAN",  "DANIEL",  "WEICHEN",  "MAYA",  "ELIZABETH",  "ANDREW",  "VANSH",  "JONATHAN",  "ABID",  "WILLIAM",  "HUI",  "ANSON",  "KEVIN",  "DANIEL",  "IVAN",  "JASMINE",  "JEFFREY", "Ruiwen"],
+    7:["DIANA",  "DAVID",  "SAM",  "PRATTAY",  "ANNA",  "JING YI",  "ADEN",  "EMERSON",  "RUSSELL",  "JACOB",  "WILLIAM",  "NADA",  "SAMANTHA",  "IAN",  "MARC",  "ANJINI",  "JEREMY",  "LAUREN",  "KEVIN",  "RAVINDRA",  "SADI",  "EMILY",  "GITAE",  "MAY",  "MAHIR",  "VIVIAN",  "GABRIEL",  "BRIANNA",  "JUN HONG",  "JOSEPH",  "MATTHEW",  "JAMES",  "THOMAS",  "NICOLE",  "Karen"],
+    8:["ALEKSANDRA",  "NAKIB",  "AMEER",  "HENRY",  "DONALD",  "YAT LONG",  "SEBASTIAN",  "DAVID",  "YUKI",  "SHAFIUL",  "DANIEL",  "SELENA",  "JOSEPH",  "SHINJI",  "RYAN",  "APRIL",  "ERICA",  "JIAN HONG",  "VERIT",  "JOSHUA",  "WILSON",  "AAHAN",  "GORDON",  "JUSTIN",  "MAYA",  "FAIYAZ",  "SHREYA",  "ERIC",  "JEFFERY",  "BRIAN",  "KEVIN",  "SAMSON",  "BRIAN",  "HARRY",  "Wanying", "Kevin"]
+}
+
+#print(choose(krewes1))
+print(choose(krewes))
+addPd(3, krewes)
+print(choose(krewes))
+addStu("mathias", 3, krewes)
+print(krewes[3])
+print(rmStu("APRIL", 8, krewes))
+print(rmStu("APRIL", 8, krewes))
+print(krewes[8])
---
+++ 05_bitstream/krewes.py
@@ -0,0 +1,102 @@
+'''
+Trio: April(horanghae), Wanying(pancake), Erica(hugo)  
+Soft Dev
+K05 -- Random Devo and Ducky from Krewes
+2022-09-28
+time spent: 1 hr 
+DISCO: 
+Split string with split() and its uses for it  
+
+QCC:
+How to split dictionaries
+How to import files and read it 
+How to find index of certain characters in strings 
+
+OPS SUMMARY:
+If krewes is empty, return ERROR
+Gets a list of the keys in the krewes
+Randomly iterates through all the list of keys to determine whether it contains values or not. If the key is empty, it will remove the key and continue on. If all the keys’ lists are empty, return ERROR. 
+When one of the keys is found to have a non-empty list, this key is chosen. 
+Choose a random index from list associated with chosen key
+Return value at the random index of the associated key.
+
+New Part: 
+Function to change string into a dictionary. 
+
+To choose ducky, we will create another dictionary with identical index values of the person it is connected with.
+Now, we will return the period and index of the person.
+With those values we will select the corresponding ducky
+
+
+'''
+
+from random import randint
+
+krewes_file = open("krewes.txt", "r") 
+content = krewes_file.read() #read in file and make it a string
+
+names_dictionary = {}
+ducky_dictionary = {}
+
+def add_pd(pd, dictionary):
+    dictionary[pd] = []
+
+def rm_pd(pd, dictionary):
+    return dictionary.pop(pd)
+
+def add_stu(name, pd, dictionary):
+    periods = list(dictionary)
+    if not (pd in periods): #checks to see if the period is already in the dictionary 
+        add_pd(pd, dictionary)
+    dictionary[pd].append(name)
+    
+def rm_stu(name, pd, dictionary):
+    for e in range(len(dictionary[pd])):
+        if dictionary[pd][e] == name:
+            return dictionary[pd].pop(e)
+    return "no student with " + name + " found"
+
+def make_dictionary():
+    people = 0
+    content_list = content.split("@@@") #split string into list between @@@
+    while people < len(content_list):
+        people_list = content_list[people].split("$$$")
+        add_stu(people_list[1], people_list[0], names_dictionary)
+        add_stu(people_list[2], people_list[0], ducky_dictionary)
+        people += 1
+
+def get_random_index(dictionary):
+    if len(dictionary) == 0:
+        return "ERROR: Dictionary has no values"       
+    key = []
+    for e in dictionary.keys(): #gets all the keys
+        key.append(e)
+    while True:
+        if len(key) == 0:
+            return "ERROR: all lists empty"
+        period = key[randint(0, len(key)-1)]  #chooses a random key
+        if len(dictionary.get(period)) == 0:
+            key.remove(period)
+        else:
+            r = randint(0, len(dictionary.get(period))-1) #chooses random index of the list associated with the random key
+            break
+    return [period,r]
+
+def get_random(dictionary): #for only one dictionary 
+    vals = get_random_index(dictionary)
+    name_period = dictionary.get(vals[0]) 
+    return "Period " + vals[0] + " || " + name_period[vals[1]] 
+
+def get_random_double(dictionary1, dictionary2): #for both dictionaries
+    vals = get_random_index(dictionary1)
+    name_period = dictionary1.get(vals[0]) 
+    ducky_period = dictionary2.get(vals[0]) 
+    return "Period " + vals[0] + " || " + name_period[vals[1]] + " || " + ducky_period[vals[1]] 
+
+make_dictionary()
+print(names_dictionary)
+print(ducky_dictionary)
+
+print(get_random_double(names_dictionary, ducky_dictionary))
+print(get_random(names_dictionary))
+
